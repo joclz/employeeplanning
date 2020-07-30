acc936ed269ec2bcc0cafbd682b65ff910578a53
Jochen Wolz <jochen.wolz@cegeka.de>
2020-07-30 14:39:24 +0200

    Initial commit

---
+++ src/main/java/com/cegeka/employeeplanning/EmployeeplanningApplication.java
@@ -0,0 +1,13 @@
+package com.cegeka.employeeplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeplanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeplanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cegeka/employeeplanning/EmployeeplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cegeka.employeeplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeplanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
