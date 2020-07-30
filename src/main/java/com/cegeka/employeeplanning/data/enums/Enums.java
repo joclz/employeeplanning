7bd5daa1360edf7fa187fac1b771b863a71e7484
Jochen Wolz <jochen.wolz@cegeka.de>
2020-07-30 16:22:49 +0200

    Initialisierung Testdaten

--- src/main/java/com/cegeka/employeeplanning/data/enums/Enums.java
+++ src/main/java/com/cegeka/employeeplanning/data/enums/Enums.java
@@ -2,18 +2,48 @@ package com.cegeka.employeeplanning.data.enums;
 
 public class Enums {
     public enum MitarbeiterStatus {
-        ANGESTELLT,
-        SUBUNTERNEHMER
+        ANGESTELLT(1),
+        SUBUNTERNEHMER(2);
+
+        private final int value;
+
+        private MitarbeiterStatus(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
     public enum MitarbeiterUnit {
-        FACTORY_MUENCHEN,
-        FACTORY_NUERNBERG
+        FACTORY_MUENCHEN(1),
+        FACTORY_NUERNBERG(2);
+
+        private final int value;
+
+        private MitarbeiterUnit(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
     public enum EinsatzStatus {
-        ANGEBOTEN,
-        BEAUFTRAGT,
-        ABGELEHNT
+        ANGEBOTEN(1),
+        BEAUFTRAGT(2),
+        ABGELEHNT(3);
+
+        private final int value;
+
+        private EinsatzStatus(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 }
