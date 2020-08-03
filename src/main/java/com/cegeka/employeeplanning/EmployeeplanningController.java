6c10fae39333a7ac6be7be1dae98702058fae089
Jochen Wolz <jochen.wolz@cegeka.de>
2020-08-03 11:45:39 +0200

    listEinsaetze

--- src/main/java/com/cegeka/employeeplanning/EmployeeplanningController.java
+++ src/main/java/com/cegeka/employeeplanning/EmployeeplanningController.java
@@ -1,5 +1,7 @@
 package com.cegeka.employeeplanning;
 
+import com.cegeka.employeeplanning.data.Einsatz;
+import com.cegeka.employeeplanning.data.EinsatzRepository;
 import com.cegeka.employeeplanning.data.Mitarbeiter;
 import com.cegeka.employeeplanning.data.MitarbeiterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeplanningController {
     @Autowired
     private MitarbeiterRepository mitarbeiterRepository;
+    @Autowired
+    private EinsatzRepository einsatzRepository;
 
     @GetMapping("/listMitarbeiter")
     public Iterable<Mitarbeiter> getMitarbeiter() {
         return mitarbeiterRepository.findAll();
+    }
+
+    @GetMapping("/listEinsaetze")
+    public Iterable<Einsatz> getEinsaetze() {
+        return einsatzRepository.findAll();
     }
 }
