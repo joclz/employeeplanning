3b370de50103a52faf6a9e47da6f1e1bca6b07d1
Jochen Wolz <jochen.wolz@cegeka.de>
2020-08-07 15:42:38 +0200

    Calc Einsatz Werte

--- src/main/java/com/cegeka/employeeplanning/data/EinsatzService.java
+++ src/main/java/com/cegeka/employeeplanning/data/EinsatzService.java
@@ -1,14 +1,47 @@
 package com.cegeka.employeeplanning.data;
 
+import org.assertj.core.util.VisibleForTesting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EinsatzService {
     @Autowired
     private EinsatzRepository einsatzRepository;
 
-    public <S extends Einsatz> S save(S s) {
-        return einsatzRepository.save(s);
+    @Autowired
+    private MitarbeiterVertriebRepository mitarbeiterVertriebRepository;
+
+    @Autowired
+    private MitarbeiterRepository mitarbeiterRepository;
+
+    public <S extends Einsatz> S save(S einsatz) {
+        einsatz = calcEinsatzWerte(einsatz);
+        return einsatzRepository.save(einsatz);
+    }
+
+    @VisibleForTesting
+    public <S extends Einsatz> S calcEinsatzWerte(S einsatz) {
+        //Todo - Mitarbeiter und Mitarbeitervertrieb setzen
+        if (einsatz.getMitarbeiterVertrieb() == null) {
+            Optional<MitarbeiterVertrieb> byId = mitarbeiterVertriebRepository.findById(1);
+            einsatz.setMitarbeiterVertrieb(byId.get());
+        }
+        if (einsatz.getMitarbeiter() == null) {
+            Optional<Mitarbeiter> byId1 = mitarbeiterRepository.findById(1);
+            einsatz.setMitarbeiter(byId1.get());
+        }
+
+        double stundensatzEK = 0.;
+        if (einsatz.getMitarbeiter() != null) {
+            stundensatzEK = einsatz.getMitarbeiter().getStundensatzEK();
+        }
+        einsatz.setDeckungsbeitrag(einsatz.getStundensatzVK() - einsatz.getZusatzkostenReise() - stundensatzEK);
+        if (einsatz.getStundensatzVK() != 0) {
+            einsatz.setMarge(einsatz.getDeckungsbeitrag() / einsatz.getStundensatzVK());
+        }
+        return einsatz;
     }
 }
---
+++ src/test/java/com/cegeka/employeeplanning/EinsatzServiceTests.java
@@ -0,0 +1,47 @@
+package com.cegeka.employeeplanning;
+
+import com.cegeka.employeeplanning.data.Einsatz;
+import com.cegeka.employeeplanning.data.EinsatzRepository;
+import com.cegeka.employeeplanning.data.EinsatzService;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EinsatzServiceTests {
+    @Autowired
+    private EinsatzRepository einsatzRepository;
+
+    @Autowired
+    private EinsatzService einsatzService;
+
+    @Test
+    public void test_001_calcEinsatzWerteWithEinsatz1() throws Exception {
+        Optional<Einsatz> einsatzId1 = einsatzRepository.findById(1);
+        Einsatz einsatz = einsatzService.calcEinsatzWerte(einsatzId1.get());
+
+        assertThat(einsatz.getDeckungsbeitrag()).isEqualTo(50);
+        assertThat(einsatz.getMarge()).isEqualTo(0.5555555555555556);
+    }
+
+    @Test
+    public void test_001_calcEinsatzWerteWithEinsatz2() throws Exception {
+        Optional<Einsatz> einsatzId1 = einsatzRepository.findById(2);
+        Einsatz einsatz = einsatzService.calcEinsatzWerte(einsatzId1.get());
+
+        assertThat(einsatz.getDeckungsbeitrag()).isEqualTo(25);
+        assertThat(einsatz.getMarge()).isEqualTo(0.25);
+    }
+}
