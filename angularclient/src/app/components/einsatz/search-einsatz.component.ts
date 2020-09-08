c53b24d2f2798a25e45049ccc2bb47b02fa0caf5
cegekaSchweiger <philipp.schweiger@cegeka.de>
2020-09-08 14:17:13 +0200

    reset button for einsatzsuche

--- angularclient/src/app/components/einsatz/search-einsatz.component.ts
+++ angularclient/src/app/components/einsatz/search-einsatz.component.ts
@@ -163,4 +163,8 @@ export class SearchEinsatzComponent implements OnInit, OnDestroy {
       this.isEinsatzAngeboten = true;
     });
   }
+
+  resetForm() {
+    this.searchFormGroup.reset();
+  }
 }
