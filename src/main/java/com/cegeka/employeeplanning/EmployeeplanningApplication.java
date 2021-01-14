f436a707960fa33d7c786318ff99bc499374e9e1
Peter von Milczewski <peter.von-milczewski@cegeka.de>
2021-01-14 12:07:47 +0100

    Html Login-Seite

--- src/main/java/com/cegeka/employeeplanning/EmployeeplanningApplication.java
+++ src/main/java/com/cegeka/employeeplanning/EmployeeplanningApplication.java
@@ -2,8 +2,9 @@ package com.cegeka.employeeplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class EmployeeplanningApplication {
 
     public static void main(String[] args) {
