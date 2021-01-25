2f90da866b9a9b749fa997270ac5a70169d29607
Matthias Hoff <matthias.hoff@cegeka.de>
2021-01-25 09:42:19 +0100

    Refactoring / Vorbereitung für Datumsfilter und Joins
    Change-Id: I399cf01921f78ee89137017bc8097a1c26c7a1b7

--- src/main/java/com/cegeka/employeeplanning/data/util/EntitySpecificationBuilder.java
+++ src/main/java/com/cegeka/employeeplanning/data/util/EntitySpecificationBuilder.java
@@ -33,11 +33,76 @@ public class EntitySpecificationBuilder<T>
                 if (index > 0) {
                     String key = str.substring(0, index);
                     String value = str.substring(index + op.getOperationName().length());
-                    return (Specification<T>) (root, query, cb) -> op.build(cb, root, key, value);
+                    String operation = op.getOperationName();
+                    {
+                        // TODO. Joins
+
+                        switch (operation){
+                        // TODO DAtums
+
+                        case "::":
+                           return (Specification<T>) (root, query, cb) ->
+                              cb.equal(root.get(key),
+                               castToRequiredType(root.get(key).getJavaType(),
+                                                  value.toString()));
+
+                        case ">":
+                           return (Specification<T>) (root, query, cb) ->
+                              cb.gt(root.get(key),
+                               (Number) castToRequiredType(
+                                      root.get(key).getJavaType(),
+                                      value.toString()));
+
+                        case ">=":
+                            return (Specification<T>) (root, query, cb) ->
+                               cb.ge(root.get(key),
+                                (Number) castToRequiredType(
+                                       root.get(key).getJavaType(),
+                                       value.toString()));
+
+                        case "<":
+                           return (Specification<T>) (root, query, cb) ->
+                              cb.lt(root.get(key),
+                               (Number) castToRequiredType(
+                                      root.get(key).getJavaType(),
+                                      value.toString()));
+
+                        case "<=":
+                            return (Specification<T>) (root, query, cb) ->
+                               cb.le(root.get(key),
+                                (Number) castToRequiredType(
+                                       root.get(key).getJavaType(),
+                                       value.toString()));
+
+                        case ":>":
+                          return (Specification<T>) (root, query, cb) ->
+                              cb.like(root.get(key),
+                                              "%"+value.toString()+"%");
+
+
+                        default:
+                          throw new RuntimeException("Operation not supported yet");
+                        }
+                    }
+
+
+//                    return (Specification<T>) (root, query, cb) -> op.build(cb, root, key, value);
                 }
             }
 
             return null;
         }).filter(s -> s != null).collect(Collectors.toList());
     }
+
+    private static Object castToRequiredType(Class fieldType, String value) {
+        if(fieldType.isAssignableFrom(Double.class)) {
+          return Double.valueOf(value);
+        } else if(fieldType.isAssignableFrom(Integer.class)) {
+          return Integer.valueOf(value);
+        } else if(Enum.class.isAssignableFrom(fieldType)) {
+          return Enum.valueOf(fieldType, value);
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/cegeka/employeeplanning/data/util/FilterOperation.java
+++ src/main/java/com/cegeka/employeeplanning/data/util/FilterOperation.java
@@ -1,70 +1,24 @@
 package com.cegeka.employeeplanning.data.util;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
-
 public enum FilterOperation
 {
     // @formatter:off
-    LESS_EQUAL_THAN("<=", (b, k, v) -> b.lessThanOrEqualTo(k, v.toString())),
-    GREATER_EQUAL_THAN(">=", (b, k, v) -> b.greaterThanOrEqualTo(k, v.toString())),
-    CONTAINS(":>", (b, k, v) -> b.like(k, b.literal("%" + v + "%"))),
-    GREATER_THAN(">", (b, k, v) -> b.greaterThan(k, v.toString())),
-    LESS_THAN("<", (b, k, v) -> b.lessThan(k, v.toString())),
-    EQUALS("::", (b, k, v) -> b.equal(k, v));
+    LESS_EQUAL_THAN("<="),
+    GREATER_EQUAL_THAN(">="),
+    CONTAINS(":>"),
+    GREATER_THAN(">"),
+    LESS_THAN("<"),
+    EQUALS("::");
     // @formatter:on
 
     private final String operationName;
-    private final FilterPredicateFunction operation;
 
-    FilterOperation(String operationName, FilterPredicateFunction operation) {
+    FilterOperation(String operationName /*, FilterPredicateFunction operation*/) {
         this.operationName = operationName;
-        this.operation = operation;
     }
 
     public String getOperationName() {
         return operationName;
     }
 
-    public Predicate build(CriteriaBuilder builder, Root<?> entity, String key, Object value) {
-        return operation.predicate(builder, entity.get(key), castToRequiredType(entity.get(key).getJavaType(),value.toString()));
-    }
-
-    static FilterOperation parse(String str) {
-        for (FilterOperation filter : FilterOperation.values()) {
-            if (str.equals(filter.getOperationName())) {
-                return filter;
-            }
-        }
-
-        throw new RuntimeException(String.format("Filter operation '%s' not found", str));
-//        throw new WrongFilterException(String.format("Filter operation '%s' not found", str));
-    }
-
-    private Object castToRequiredType(Class fieldType, String value) {
-        if(fieldType.isAssignableFrom(Double.class)) {
-          return Double.valueOf(value);
-        } else if(fieldType.isAssignableFrom(Integer.class)) {
-          return Integer.valueOf(value);
-        } else if(Enum.class.isAssignableFrom(fieldType)) {
-          return Enum.valueOf(fieldType, value);
-        }
-        return null;
-    }
-
-//    private Object castToRequiredType(Class fieldType, List<String> value) {
-//        List<Object> lists = new ArrayList<>();
-//        for (String s : value) {
-//          lists.add(castToRequiredType(fieldType, s));
-//        }
-//        return lists;
-//    }
-
-    @FunctionalInterface
-    interface FilterPredicateFunction {
-        Predicate predicate(CriteriaBuilder builder, Path<String> key, Object value);
-    }
 }
