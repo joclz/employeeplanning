43cbd01a1e3f70fea06a1e663de5ea5149740bf7
Jochen Wolz <jochen.wolz@cegeka.de>
2021-03-03 13:27:07 +0100

    SeleniumTest testAddMitarbeiter

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -1,29 +1,47 @@
 package selenium;
 
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class SeleniumTest {
     public static final int TIMEOUT_VERY_SHORT = 100;
     public static final int TIMEOUT_SHORT = 250;
     public static final int TIMEOUT_MIDDLE = 500;
     public static final int TIMEOUT_LONG = 1000;
 
-    public static void main(String[] args) {
+    private WebDriver driver;
+    private String baseUrl = "http://localhost:4200/employeeplanning#/";
 
+    @Before
+    public void setUp(){
         System.setProperty("webdriver.gecko.driver", "D:\\Programme\\geckodriver-v0.29.0-win64\\geckodriver.exe");
-
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         //Maximize browser window
         driver.manage().window().maximize();
         //Go to URL which you want to navigate
-        driver.get("http://localhost:4200/employeeplanning#/");
+        driver.get(baseUrl);
         //Set  timeout  for 500 milliseconds so that the page may load properly within that time
-        sleep(driver, TIMEOUT_LONG);
+        sleep(driver, TIMEOUT_MIDDLE);
 
-        /** LOGIN
-         */
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("adminPass");
+        driver.findElement(By.id("submitLogin")).click();
+    }
+
+    @Test
+    public void testLogin() {
+        /** Logout */
+        driver.findElement(By.id("menuUser")).click();
+        sleep(driver, TIMEOUT_VERY_SHORT);
+        driver.findElement(By.id("logoutUser")).click();
+        sleep(driver, TIMEOUT_VERY_SHORT);
+
+        /** Login */
         driver.findElement(By.id("username")).sendKeys("a");
         sleep(driver, TIMEOUT_VERY_SHORT);
         driver.findElement(By.id("username")).sendKeys("d");
@@ -38,7 +56,10 @@ public class SeleniumTest {
         sleep(driver, TIMEOUT_SHORT);
         driver.findElement(By.id("submitLogin")).click();
         sleep(driver, TIMEOUT_MIDDLE);
+    }
 
+    @Test
+    public void testAddMitarbeiter() {
         /** Mitarbeiter Hinzufügen
          *  -> FindElement by id, name, xpath  */
         driver.findElement(By.id("menuMitarbeiter")).click();
@@ -51,21 +72,43 @@ public class SeleniumTest {
         sleep(driver, TIMEOUT_VERY_SHORT);
         driver.findElement(By.name("mitarbeiterVorname")).sendKeys("Norbert");
         sleep(driver, TIMEOUT_VERY_SHORT);
+
         driver.findElement(By.id("mitarbeiterStundensatz")).sendKeys("a");
         sleep(driver, TIMEOUT_VERY_SHORT);
-        driver.findElement(By.id("mitarbeiterStatus")).sendKeys("a");
-        sleep(driver, TIMEOUT_LONG);
+
+        driver.findElement(By.id("mitarbeiterStatus")).sendKeys("Angestellt");
+        sleep(driver, TIMEOUT_VERY_SHORT);
+        driver.findElement(By.id("mitarbeiterUnit")).sendKeys("Factory Nürnberg");
+        sleep(driver, TIMEOUT_SHORT);
+
+        /** Check Fehlermeldung Stundensatz */
+        WebElement mitarbeiterStundensatzError = driver.findElement(By.id("mitarbeiterStundensatzError"));
+        Assert.assertEquals(mitarbeiterStundensatzError.getText(), "Stundensatz ist ein Pflichtfeld");
+        driver.findElement(By.id("mitarbeiterStundensatz")).clear();
+        driver.findElement(By.id("mitarbeiterStundensatz")).sendKeys("75.0");
+
+        driver.findElement(By.id("mitarbeiterHinzufuegen")).click();
+        sleep(driver, TIMEOUT_SHORT);
+    }
+
+    @Test
+    public void testMyStuff() {
 
         /** Auflisten Mitarbeiter
          */
         driver.findElement(By.id("menuMitarbeiter")).click();
         sleep(driver, TIMEOUT_SHORT);
         driver.findElement(By.id("listMitarbeiter")).click();
-
-        System.setProperty("webdriver.firefox.marionette", "false");
     }
 
-    private static void sleep(WebDriver driver, long millis) {
+    @After
+    public void tearDown() {
+        sleep(driver, 1500);
+        driver.close();
+        //System.setProperty("webdriver.firefox.marionette", "false");
+    }
+
+    private void sleep(WebDriver driver, long millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
